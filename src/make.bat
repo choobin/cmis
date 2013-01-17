9f135f0b0cb148386f72b95c19fbc455873426e7
Christopher Hoobin <christopher@hoob.in>
2013-01-17 16:19:54 +1100

    Modified build scripts to deal with new paths.

--- src/make.bat
+++ src/make.bat
@@ -2,6 +2,6 @@
 
 del "cmis@moongiraffe.net.xpi"
 
-"C:\Program Files\7-zip\7z.exe" a -r -tzip cmis@moongiraffe.net.xpi bootstrap.js chrome/ chrome.manifest ChangeLog install.rdf LICENSE
+"C:\Program Files\7-zip\7z.exe" a -r -tzip cmis@moongiraffe.net.xpi bootstrap.js content/ chrome.manifest ChangeLog install.rdf LICENSE locale/ skin/
 
 "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" cmis@moongiraffe.net.xpi
