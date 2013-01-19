b48ffff86d2bc3ce72709eb70a89c66f973052f9
Christopher Hoobin <christopher@hoob.in>
2013-01-20 03:33:51 +1100

    Search for paths to 7zip and Firefox before running.

--- src/make.bat
+++ src/make.bat
@@ -2,6 +2,19 @@
 
 del "cmis@moongiraffe.net.xpi"
 
-"C:\Program Files\7-zip\7z.exe" a -r -tzip cmis@moongiraffe.net.xpi bootstrap.js content/ chrome.manifest ChangeLog install.rdf LICENSE locale/ skin/
+if exist "C:\Program Files\7-zip\7z.exe" (
+    set zip="C:\Program Files\7-zip\7z.exe"
+) else (
+    set zip="C:\Program Files (x86)\7-zip\7z.exe"
+)
 
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" cmis@moongiraffe.net.xpi
+if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+    set firefox="C:\Program Files\Mozilla Firefox\firefox.exe"
+) else (
+    set firefox="C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
+)
+
+%zip% a -r -tzip cmis@moongiraffe.net.xpi bootstrap.js content/ chrome.manifest ChangeLog install.rdf LICENSE locale/ skin/
+
+%firefox% cmis@moongiraffe.net.xpi
+
